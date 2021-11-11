3adb400a3a717491fd7dc77d188f89e6d11e141c
Dmitriy Redkin <expert.info79@gmail.com>
2021-11-11 07:55:47 +0300

    Update example_code.py

--- example_code.py
+++ example_code.py
@@ -6,23 +6,29 @@ from data_utils.filemanager import FileManager
 from data_utils.s3client import S3Client
 from data_utils.sqlclient import SQLClient
 
-def processing(config: str) -> None:
+def processing(yaml_config: str, ini_config: str) -> None:
     today = datetime.today().strftime('%Y-%m-%d')
     fm = FileManager()
 
-    dbsettings = fm.read_yaml_config(config, 'postgres')
-    cli = SQLClient(dbsettings)
+    postgres_settings = fm.read_yaml_config(yaml_config, 'postgres')
+    postgres_client = SQLClient(postgres_settings)
     query = f"SELECT * FROM cbrf.currency WHERE valute_load ='{today}';"
-    res = cli.execute_one(query, True)
-    cli.close_connection()
-    print(res)
+    currency = postgres_client.execute_one(query, True)
+    postgres_client.close_connection()
+    print(currency)
 
-    s3settings = fm.read_yaml_config(config, 's3')
+    greenplum_settings = fm.read_ini_config(ini_config, 'greenplum')
+    greenplum_client = SQLClient(greenplum_settings)
+    query = f"INSERT INTO cbrf.currency VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    greenplum_client.insert_batch(query, currency)
+    greenplum_client.close_connection()
+
+    s3settings = fm.read_yaml_config(yaml_config, 's3')
     s3cli = S3Client(s3settings)
     s3files = s3cli.get_info_from_s3('zip')
     print(s3files)
 
-
 if __name__ == '__main__':
-    config = 'config.yaml'
-    processing(config)
+    yaml_config, ini_config = 'config.yaml', 'config.ini'
+    processing(yaml_config, ini_config)
+
